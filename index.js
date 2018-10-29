cc87def2edaa3d836229c2aa3ce564efcd60a9c0
Stephanie Phillips <steph@ladymacbeth.uk>
2018-10-29 19:01:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   document.querySelector("div#hidden-div").innerText='301;'
 })
-*/
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById("div#hidden-div").innerHTML = '301';
+})
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.open("text/html","replace");
+    document.write("<h2>Learning about the HTML DOM is fun!</h2>");
+    document.close();
 })
