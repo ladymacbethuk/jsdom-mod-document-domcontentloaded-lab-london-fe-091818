93f35d5f9209a5c4e3567dba078c2ef951aba377
Stephanie Phillips <steph@ladymacbeth.uk>
2018-10-29 18:55:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   document.querySelector("div#hidden-div").innerText='301;'
 })
+
+
+
+document.getElementById(id).innerHTML = new HTML
